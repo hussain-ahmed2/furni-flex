791d2208a5b53c773abe28fc3cecb219f02cbb47
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-21 20:09:32 +0600

    create products router and page

---
+++ src/app/products/page.js
@@ -0,0 +1,11 @@
+import SideBar from "@/components/products/sidebar";
+
+export default function Products() {
+	return (
+		<main className="max-w-7xl mx-auto px-5 py-14">
+			<div>
+				<SideBar />
+			</div>
+		</main>
+	);
+}
