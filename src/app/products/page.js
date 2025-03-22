a806b86987b1b3aea0bbd5967656bab5dff5cb01
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-22 23:00:52 +0600

    add sidebar and product grid

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,11 +1,26 @@
+import Header from "@/components/header";
+import Pagination from "@/components/products/pagination";
+import ProductGrid from "@/components/products/product-grid";
 import SideBar from "@/components/products/sidebar";
 
 export default function Products() {
+   
 	return (
-		<main className="max-w-7xl mx-auto px-5 py-14">
-			<div>
-				<SideBar />
-			</div>
-		</main>
+		<>
+			<Header />
+			<main className="max-w-7xl mx-auto px-5 py-14">
+				<div className="flex gap-10">
+					<div>
+						<SideBar />
+					</div>
+					<div>
+						<ProductGrid />
+					</div>
+				</div>
+				<div className="mt-14 w-full">
+					<Pagination />
+				</div>
+			</main>
+		</>
 	);
 }
