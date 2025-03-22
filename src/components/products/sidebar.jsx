f7d88685689c9f9dbb339342b490172bd55ff0d8
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-22 23:01:51 +0600

    update sidebar styling

--- src/components/products/sidebar.jsx
+++ src/components/products/sidebar.jsx
@@ -1,8 +1,8 @@
 export default function SideBar() {
 	return (
-		<aside className="max-w-64 w-full pt-10 pb-16 pr-8 border-r border-gray-200">
-			<ul className="flex flex-col gap-3 w-full">
-				<li className="bg-black text-white hover:bg-cyan-500 tc active:ring-4 ring-cyan-500/30  rounded-lg text-xl font-semibold px-6 py-3">
+		<aside className="w-64 pt-10 pb-16 pr-8 border-r border-gray-200 h-full">
+			<ul className="flex flex-col gap-3 w-full text-gray-500">
+				<li className="bg-black text-white hover:bg-cyan-600 tc active:ring-4 ring-cyan-500/30  rounded-lg text-xl font-semibold px-6 py-3">
 					Rocking chair
 				</li>
 				<li className="font-medium text-xl px-6 py-3 hover:bg-gray-200 rounded-lg tc">
