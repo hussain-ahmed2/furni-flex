48857df25c7320be58eec46a0502048ddf05717c
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-04-11 18:35:40 +0600

    add links for active pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,23 @@
-import Header from "@/components/header";
+import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Home() {
-
 	return (
 		<>
-			<Header />
-			<main className="max-w-7xl mx-auto px-5">home page</main>
+			<Layout>
+				<main className="max-w-7xl mx-auto px-5">
+					<h1 className="font-bold text-3xl text-center my-5">Welcome to FurniFlex.</h1>
+
+					<p className="text-center mb-3 text-lg font-semibold">Links to the All Pages</p>
+					<div className="mb-10 flex flex-col w-fit mx-auto gap-2">
+						<Link className="font-medium text-sky-600 hover:underline" href='/'>Home Page</Link>
+						<Link className="font-medium text-sky-600 hover:underline" href='/products'>Products Page</Link>
+						<Link className="font-medium text-sky-600 hover:underline" href='/cart'>Cart Page</Link>
+						<Link className="font-medium text-sky-600 hover:underline" href='/signup'>SignUp Page</Link>
+						<Link className="font-medium text-sky-600 hover:underline" href='/login'>Login Page</Link>
+					</div>
+				</main>
+			</Layout>
 		</>
 	);
 }
