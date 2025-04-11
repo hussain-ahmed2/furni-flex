c5d9384d602a567e9343575d86fd4d17c9a9a5b1
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-04-11 18:37:05 +0600

    update css classes for responsive layout

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,28 +1,26 @@
-import Footer from "@/components/footer";
-import Header from "@/components/header";
+import Layout from "@/components/Layout";
 import Pagination from "@/components/products/pagination";
 import ProductGrid from "@/components/products/product-grid";
 import SideBar from "@/components/products/sidebar";
 
 export default function Products() {
-   
 	return (
 		<>
-			<Header />
-			<main className="max-w-7xl mx-auto px-5 py-14">
-				<div className="flex gap-10">
-					<div>
-						<SideBar />
+			<Layout>
+				<main className="max-w-7xl mx-auto px-5 py-14">
+					<div className="flex md:gap-10 max-md:flex-col">
+						<div>
+							<SideBar />
+						</div>
+						<div>
+							<ProductGrid />
+						</div>
 					</div>
-					<div>
-						<ProductGrid />
+					<div className="mt-14 w-full">
+						<Pagination />
 					</div>
-				</div>
-				<div className="mt-14 w-full">
-					<Pagination />
-				</div>
-			</main>
-			<Footer />
+				</main>
+			</Layout>
 		</>
 	);
 }
